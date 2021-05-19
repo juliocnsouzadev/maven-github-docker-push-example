e11924b34b30f6db080a6ea30b2e9d66b792bea9
juliocnsouzadev <juliocnsouzadev@gmail.com>
2021-05-19 14:35:03 -0300

    project startup

---
+++ src/main/java/br/com/juliocnsouza/apisample/ApiSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliocnsouza.apisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juliocnsouza/apisample/ApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliocnsouza.apisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
